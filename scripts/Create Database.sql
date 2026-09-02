05e1b921a51917fbbe2ca85b2f41eec0b6212846
Surajkumar Bevnale <surajk.b168@gmail.com>
2026-09-02 13:53:18 +0530

    Update Create Database.sql

--- scripts/Create Database.sql
+++ scripts/Create Database.sql
@@ -1,1 +1,27 @@
+-- This will check if the database exists and drops and then create a database DataWarehouse and then create schemas 'bronze', 'silver' and 'gold'
 
+
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
